0f592e8798f6ed8851b536bc28e04df5952835ca
Jessica Hong <jessicahong.24@gmail.com>
2019-04-13 14:47:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,6 +8,6 @@ end
 
 def assign_rooms (attendees)
  
-  attendees.each_with_index { |name,index| "Hello, #{name}! You'll be assigned to room #{index}!"}
+  attendees.each_with_index.map { |name,index| "Hello, #{name}! You'll be assigned to room #{index}!"}
   
 end
