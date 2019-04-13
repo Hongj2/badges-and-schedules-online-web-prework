f94ca9652bd083cd92bb6ff080a0b60993da2d41
Jessica Hong <jessicahong.24@gmail.com>
2019-04-13 14:46:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def badge_maker (name)
+   return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (attendees)
+  attendees.map { |name| "Hello, my name is #{name}." }
+end
+
+def assign_rooms (attendees)
+ 
+  attendees.each_with_index { |name| "Hello, #{name}! You'll be assigned to room #{index}!"}
+  
+end
