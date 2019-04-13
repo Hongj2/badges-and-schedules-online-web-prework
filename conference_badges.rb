482080815582311c8758e9d78f576142d261341f
Jessica Hong <jessicahong.24@gmail.com>
2019-04-13 14:54:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker (name)
 end
 
 def batch_badge_creator (attendees)
-  attendees.collect { |name| badge_maker (name)
+  attendees.collect { |name| badge_maker (name)}
 end
 
 def assign_rooms (attendees)
